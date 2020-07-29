b588613490c132606dfc0d3cfbd743b5fddcda86
jacobwicks <jacob.m.wicks@gmail.com>
2020-07-29 14:10:43 -0700

    wtf heroku

--- makeImage.ts
+++ makeImage.ts
@@ -1,21 +1,18 @@
 const { createCanvas } = require("canvas");
 
 const makeTextImage = async (input: string) => {
-  const canvas = createCanvas(200, 200);
-  const ctx = canvas.getContext("2d");
-  var text = ctx.measureText(input);
-
-  ctx.font = "30px Impact";
-  ctx.fillText(input, 50, 100, text.width + 50);
-
-  // Draw line under text
-  ctx.strokeStyle = "rgba(0,0,0,0.5)";
-  ctx.beginPath();
-  ctx.lineTo(50, 102);
-  ctx.lineTo(50 + text.width, 102);
-  ctx.stroke();
-
-  return canvas.toBuffer();
+  // const canvas = createCanvas(200, 200);
+  // const ctx = canvas.getContext("2d");
+  // var text = ctx.measureText(input);
+  // ctx.font = "30px Impact";
+  // ctx.fillText(input, 50, 100, text.width + 50);
+  // // Draw line under text
+  // ctx.strokeStyle = "rgba(0,0,0,0.5)";
+  // ctx.beginPath();
+  // ctx.lineTo(50, 102);
+  // ctx.lineTo(50 + text.width, 102);
+  // ctx.stroke();
+  // return canvas.toBuffer();
 };
 
 export default makeTextImage;
