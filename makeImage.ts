e4e1c0ac55da9f0677e5d7581ecf606720124518
jacobwicks <jacob.m.wicks@gmail.com>
2020-07-29 14:14:11 -0700

    ARGH

--- makeImage.ts
+++ makeImage.ts
@@ -1,18 +1,18 @@
 const { createCanvas } = require("canvas");
 
-async function makeTextImage(input: string) {
+async function makeTextImage(input) {
   const canvas = createCanvas(200, 200);
-  // const ctx = canvas.getContext("2d");
-  // var text = ctx.measureText(input);
-  // ctx.font = "30px Impact";
-  // ctx.fillText(input, 50, 100, text.width + 50);
-  // // Draw line under text
-  // ctx.strokeStyle = "rgba(0,0,0,0.5)";
-  // ctx.beginPath();
-  // ctx.lineTo(50, 102);
-  // ctx.lineTo(50 + text.width, 102);
-  // ctx.stroke();
-  // return canvas.toBuffer();
+  const ctx = canvas.getContext("2d");
+  var text = ctx.measureText(input);
+  ctx.font = "30px Impact";
+  ctx.fillText(input, 50, 100, text.width + 50);
+  // Draw line under text
+  ctx.strokeStyle = "rgba(0,0,0,0.5)";
+  ctx.beginPath();
+  ctx.lineTo(50, 102);
+  ctx.lineTo(50 + text.width, 102);
+  ctx.stroke();
+  return canvas.toBuffer();
 }
 
 export default makeTextImage;
